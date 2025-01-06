bf4fcfc8bd17a43953222adc23f47c30e1996a27
Neem-Sheth <neemsheth2003@gmail.com>
2025-01-07 02:07:40 +0530

    First Push

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+const Profile = () => {
+    
+   return (
+           
+           <View>
+             <Text> textInComponent </Text>
+           </View>
+         )
+}
+
+export default Profile;
+
